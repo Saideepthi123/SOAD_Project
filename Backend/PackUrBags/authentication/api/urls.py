747f72fb92d96ef315db1b13a3af8dba3fe046b5
nagaharshita <harshitamarupaka@gmail.com>
2020-10-28 13:11:17 +0530

    made changes

--- Backend/PackUrBags/authentication/api/urls.py
+++ Backend/PackUrBags/authentication/api/urls.py
@@ -2,8 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('authentication.api.urls')),
-    path('api/guide/', include('guide.api.urls')),
-    path('api/user/', include('tourism.api.urls')),
+    
 ]
