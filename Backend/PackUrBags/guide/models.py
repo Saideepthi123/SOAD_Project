ac023d17181d6618ec8d5f8cf328baed5a3ed6aa
Saideepthi123 <saideepthi.k18@iiits.in>
2020-10-28 15:19:05 +0530

    Updated requirements.txt and Guide model

--- Backend/PackUrBags/guide/models.py
+++ Backend/PackUrBags/guide/models.py
@@ -2,6 +2,9 @@ from djongo import models
 from datetime import datetime
 
 # Create your models here.
+class Place(models.Model):
+    place = models.CharField(max_length=50)
+
 class GuideData(models.Model):
     guide_id = models.AutoField(primary_key=True)
     email = models.EmailField(verbose_name="email", unique=True)
@@ -9,6 +12,7 @@ class GuideData(models.Model):
     last_name = models.CharField(max_length=100)
     username = models.CharField(max_length=200, unique=True)
     phone_number = models.CharField(max_length=10, unique=True)
+    place      = models.ManyToManyField(Place)
     profile_pic = models.ImageField(default='no_image.png', upload_to='guide_profile_pics/')
     dob = models.DateField(default=datetime.today)
     address = models.TextField()
