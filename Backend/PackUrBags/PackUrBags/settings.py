82a36b870f16bbf1fa2b11f2e34e37027ab359c4
abhinaybathina <abhinaybathina2@gmail.com>
2020-10-31 12:01:10 +0530

    SimpleJWT

--- Backend/PackUrBags/PackUrBags/settings.py
+++ Backend/PackUrBags/PackUrBags/settings.py
@@ -108,9 +108,6 @@ AUTHENTICATION_BACKENDS = [
 
 REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
 }
 
 # Password validation
