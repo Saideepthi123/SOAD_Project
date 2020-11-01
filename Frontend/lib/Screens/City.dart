1969db3b2b857466dac25d2ce97f9ffe7eebc5b1
unknown <vitthal.inani@gmail.com>
2020-11-01 12:48:09 +0530

    City Screen

--- Frontend/lib/Screens/City.dart
+++ Frontend/lib/Screens/City.dart
@@ -9,9 +9,48 @@ class CityPage extends StatefulWidget {
 class _CityPageState extends State<CityPage> {
   @override
   Widget build(BuildContext context) {
+    var _screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: TopAppBar(context),
-      body: Container(),
+      body: Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Material(
+              elevation: 10,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    image: DecorationImage(
+                        image: AssetImage("img/ImageDelhi.jpg"),
+                        fit: BoxFit.fill)),
+                width: _screenSize.width * 0.4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("City Name"), Text("City Info")],
+                ),
+              ),
+            ),
+            DefaultTabController(
+              length: 5,
+              child: Container(
+                color: Colors.orangeAccent,
+                width: _screenSize.width * 0.6,
+                child: TabBar(
+                  indicatorColor: Colors.white,
+                  tabs: [
+                    Tab(text: "Visit"),
+                    Tab(text: "Guides"),
+                    Tab(text: "Food"),
+                    Tab(text: "Travel"),
+                    Tab(text: "Stay"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- Frontend/lib/Tools/Global tools.dart
+++ Frontend/lib/Tools/Global tools.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel/Screens/City.dart';
 import '../Screens/HomePage.dart';
 
 class TopAppBar extends AppBar {
   BuildContext context;
+
   @override
   // TODO: implement leading
   Widget get leading => IconButton(
-    icon: Icon(FontAwesomeIcons.plane),
-    onPressed: () {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => HomePage()));
-    },
-  );
+        icon: Icon(FontAwesomeIcons.plane),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => HomePage()));
+        },
+      );
+
+  @override
+  Widget get title => InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => CityPage()));
+      },
+      child: Text("Hello"));
+
   TopAppBar(this.context);
 }
