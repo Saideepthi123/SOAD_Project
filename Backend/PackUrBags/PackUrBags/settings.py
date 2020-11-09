85214efabff544197d7c52088b94eaa2ffcc4077
nagaharshita <harshitamarupaka@gmail.com>
2020-11-09 22:29:49 +0530

    added cors-headers

--- Backend/PackUrBags/PackUrBags/settings.py
+++ Backend/PackUrBags/PackUrBags/settings.py
@@ -55,8 +55,20 @@ INSTALLED_APPS = [
 
 if DEBUG is True:
     INSTALLED_APPS += ('corsheaders', )
-    CORS_ORIGIN_ALLOW_ALL = True
-    CORS_ALLOW_HEADERS = default_headers + ('http://localhost:55065')
+    CORS_ALLOW_ALL_ORIGINS = True
+    CORS_ALLOW_HEADERS = [
+        'accept',
+        'accept-encoding',
+        'authorization',
+        'content-type',
+        'dnt',
+        'origin',
+        'user-agent',
+        'x-csrftoken',
+        'x-requested-with',
+    ]
+    CORS_ALLOW_CREDENTIALS = True
+   
 
 SITE_ID = 1
 SOCIALACCOUNT_QUERY_EMAIL = True
