a0202577c7165cb557acc8d8a492abd20b77ac0b
Manasa PVS <venkatasrimanasa.p18@iiits.in>
2020-11-22 17:05:03 +0530

    city/monument changes

--- Backend/PackUrBags/monuments/api/serializers.py
+++ Backend/PackUrBags/monuments/api/serializers.py
@@ -5,7 +5,7 @@ from monuments.models import Monument, MonumentInfo, City
 class MonumentDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Monument
-        fields = ('monument_id', 'monument_name', 'country', 'basic_info', 'city')
+        fields = ('monument_id', 'monument_name', 'country', 'basic_info', 'in_city','imageURL','pin_code')
 
     def create(self, validated_data):
         try:
@@ -17,10 +17,11 @@ class MonumentDataSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.monument_id = validated_data.get('monument_id', instance.monument_id)
         instance.monument_name = validated_data.get('monument_name ', instance.monument_name)
-        instance.city = validated_data.get('city', instance.city)
+        instance.city = validated_data.get('in_city', instance.city)
         instance.country = validated_data.get('country', instance.country)
         instance.basic_info = validated_data.get('basic_info', instance.basic_info)
         instance.pin_code = validated_data.get('pin_code', instance.pin_code)
+        instance.imageURL= validated_data.get('imageURL', instance.imageURL)
         instance.save()
         return super().update(instance, validated_data)
 
@@ -28,7 +29,7 @@ class MonumentDataSerializer(serializers.ModelSerializer):
 class MonumentInfoDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = MonumentInfo
-        fields = ('monument_info_id', 'monument_name', 'category', 'info')
+        fields = ('monument_info_id', 'monument_name', 'category', 'info','imageURL')
 
     def create(self, validated_data):
         try:
@@ -41,6 +42,7 @@ class MonumentInfoDataSerializer(serializers.ModelSerializer):
         instance.monument_info_id = validated_data.get('monument_info_id', instance.monument_info_id)
         instance.monument_name = validated_data.get('monument_name ', instance.monument_name)
         instance.info = validated_data.get('info', instance.info)
+        instance.imageURL= validated_data.get('info', instance.imageURL)
         instance.category = validated_data.get('category', instance.category)
         instance.save()
         return super().update(instance, validated_data)
@@ -49,7 +51,7 @@ class MonumentInfoDataSerializer(serializers.ModelSerializer):
 class CityDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = ('city_name', 'state', 'country', 'pin_code')
+        fields = ('city_name', 'state', 'country', 'pin_code','imageURL','city_info','monuments')
 
     def create(self, validated_data):
         try:
@@ -64,5 +66,8 @@ class CityDataSerializer(serializers.ModelSerializer):
         instance.state = validated_data.get('state', instance.state)
         instance.country = validated_data.get('country', instance.country)
         instance.pin_code = validated_data.get('pin_code', instance.pin_code)
+        instance.imageURL=validated_data.get('imageURL', instance.imageURL)
+        instance.city_info=validated_data.get('city_info', instance.city_info)
+        instance.monuments=validated_data.get('monuments',instance.monuments)
         instance.save()
         return super().update(instance, validated_data)
--- Backend/PackUrBags/monuments/models.py
+++ Backend/PackUrBags/monuments/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 
 class City(models.Model):
     city_id = models.AutoField(primary_key=True)
@@ -9,22 +8,29 @@ class City(models.Model):
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     pin_code = models.CharField(max_length=6)
+    city_info= models.TextField(default="city info")
+    imageURL=models.TextField(default="https://img.freepik.com/free-vector/cityscape-cartoon-background-panorama-modern-city-with-high-skyscrapers-park-downtown_1441-1982.jpg?size=626&ext=jpg")
+    monuments = models.ManyToManyField('Monument')
 
     def __str__(self):
         return self.city_name
 
-
+# Create your models here.
 class Monument(models.Model):
     monument_id = models.AutoField(primary_key=True)
     monument_name = models.CharField(max_length=100)
-    place = models.CharField(max_length=100)
+    # place = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     basic_info = models.TextField()
     pin_code = models.CharField(max_length=6)
-    city = models.ManyToManyField(City)
+    imageURL=models.TextField(default="https://img.freepik.com/free-vector/cityscape-cartoon-background-panorama-modern-city-with-high-skyscrapers-park-downtown_1441-1982.jpg?size=626&ext=jpg")
+    in_city = models.ForeignKey(City,on_delete=models.CASCADE,default=1)
 
     def __str__(self):
         return self.monument_name
+
+
+
 
 
 class MonumentInfo(models.Model):
@@ -32,6 +38,7 @@ class MonumentInfo(models.Model):
     monument_name = models.ForeignKey(Monument, on_delete=models.CASCADE)
     category = models.CharField(max_length=100)
     info = models.TextField()
+    imageURL=models.TextField(default="https://img.freepik.com/free-vector/cityscape-cartoon-background-panorama-modern-city-with-high-skyscrapers-park-downtown_1441-1982.jpg?size=626&ext=jpg")
 
     def __str__(self):
         return f'{self.monument_name}\'s info'
