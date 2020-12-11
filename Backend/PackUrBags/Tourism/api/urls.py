0acdf6050029767e9d830253d7ecaec22362dc68
nagaharshita <harshitamarupaka@gmail.com>
2020-12-11 20:59:02 +0530

    Modified urls

--- Backend/PackUrBags/Tourism/api/urls.py
+++ Backend/PackUrBags/Tourism/api/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('skyscanner/search-flights', views.SkyScannerSearchFlights.as_view(), name="search-flights"),
     path('skyscanner/flight-routes', views.SkyScannerFlightRoutes.as_view(), name="browse-routes"),
 
-    path('checkout/', views.Stripe.as_view(), name='payment-stripe'),
+    
 ]
+
